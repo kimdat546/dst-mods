7ec7646b03cb26af48a507e79a503283108ac5dc
kimdat546 <ba.pham@saigontechnology-sdc.space>
2026-05-30 22:14:49 +0700

    feat(remake): recover UI/character assets + main/assets + main/strings

---
+++ scripts/main/assets.lua
@@ -0,0 +1,23 @@
+PrefabFiles = {
+    "phamnhan",
+}
+
+Assets = {
+    Asset("ATLAS", "images/pn_ui.xml"),
+    Asset("IMAGE", "images/pn_ui.tex"),
+
+    Asset("IMAGE", "images/saveslot_portraits/phamnhan.tex"),
+    Asset("ATLAS", "images/saveslot_portraits/phamnhan.xml"),
+    Asset("IMAGE", "bigportraits/phamnhan.tex"),
+    Asset("ATLAS", "bigportraits/phamnhan.xml"),
+    Asset("IMAGE", "images/map_icons/phamnhan.tex"),
+    Asset("ATLAS", "images/map_icons/phamnhan.xml"),
+    Asset("IMAGE", "images/avatars/avatar_phamnhan.tex"),
+    Asset("ATLAS", "images/avatars/avatar_phamnhan.xml"),
+    Asset("IMAGE", "images/avatars/avatar_ghost_phamnhan.tex"),
+    Asset("ATLAS", "images/avatars/avatar_ghost_phamnhan.xml"),
+    Asset("IMAGE", "images/names_phamnhan.tex"),
+    Asset("ATLAS", "images/names_phamnhan.xml"),
+}
+
+AddMinimapAtlas("images/map_icons/phamnhan.xml")
